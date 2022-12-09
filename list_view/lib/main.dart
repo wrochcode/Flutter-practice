2d44e30c5e53311b672e3e2be468d61227d62abd
Wahyu Rochman <wrochcode@gmail.com>
2022-12-09 20:33:41 +0700

    update

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -1,141 +1,111 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp({super.key});
 
+  // List<Container> myList = [ //widget container aja
+  final List<Widget> myList = [
+    // all widget
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.red,
+    ),
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.green,
+    ),
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.blue,
+    ),
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.black,
+    ),
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.white,
+    ),
+    Container(
+      height: 125,
+      width: 100,
+      color: Colors.purple,
+    ),
+  ];
+
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.black,
+  ];
+
+  final List<Widget> myNumber = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(
+          // fontSize: 20 + double.parse(index.toString()),//tambahan
+          ),
+      // index.toString(),
+    ),
+  );
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("List View"),
+          title: Center(
+            child: Text(
+              "List View",
+            ),
+          ),
         ),
         body: ListView(
-          // scrollDirection: Axis.horizontal,
-          children: [
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.red,
-            ),
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.green,
-            ),
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.blue,
-            ),
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.black,
-            ),
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.white,
-            ),
-            Container(
-              height: 125,
-              width: 100,
-              color: Colors.purple,
-            ),
-          ],
+          children: myNumber,
         ),
+        // listview separated:
+        // body: ListView.separated(//----------------------------------------------------------------------------
+        //   separatorBuilder: (context, index) {
+        //     return Divider(
+        //       color: Colors.black,
+        //       // height: 40,
+        //     );
+        //     // return Container(
+        //     //   height: 10,
+        //     // );
+        //   },
+        //   itemCount: 10,
+        //   itemBuilder: (context, index) {
+        //     return Text("Hallo");
+        //     // return Container(
+        //     //   height: 300,
+        //     //   width: 300,
+        //     //   color: myColor[index],
+        //     // );
+        //   },
+
+        // scrollDirection: Axis.horizontal,
+
+        // ),
+
+        //maually:-----------------------------------------------------------------------------
+        // body: ListView(
+        //   children: myList,
+        // ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
